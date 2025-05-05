ea8265e15b9a09a917a5cf0ce50c975b9f6475a7
Winnie Mutheu <wmutheu@konza.go.ke>
2025-05-05 16:03:42 +0300

    updates

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -124,7 +124,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
